71b1a9e606bf512f7bf4d3e3929dea034d083dfc
Hüseyin Yiğit <ruzgiarr@gmail.com>
2023-03-01 18:24:40 +0300

    Update IN - BETWEEN.sql

--- IN - BETWEEN.sql
+++ IN - BETWEEN.sql
@@ -1,5 +1,4 @@
 
-
 --- 1. film tablosunda bulunan tüm sütunlardaki verileri replacement cost değeri 12.99 dan büyük eşit ve 16.99 küçük olma koşuluyla sıralayınız ( BETWEEN - AND yapısını kullanınız.)
 
 SELECT replacement_cost FROM film
